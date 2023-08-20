124898dd51ee194d6f5e6f5609c37908db1fefa5
Hussein Dabs <fifa_street_3000@hotmail.com>
2023-08-20 19:34:35 -0400

    ax.set(label)

--- rw_visual.py
+++ rw_visual.py
@@ -10,17 +10,23 @@ while True:
 
     # Plotting the point for the walk
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+
+    # To fit the data more nicely, we can add inside plot.sublots() this => figsize=(15, 10)
+    fig, ax = plt.subplots(figsize=(11, 8))
+
     # range to generate a list of # equal to # of point in the walk
     point_number = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_number,
-               cmap=plt.cm.Blues, edgecolors='none', s=12)  # edgecolors to get ride of the black outline around the points
+               cmap=plt.cm.Blues, edgecolors='none', s=5)  # edgecolors to get ride of the black outline around the points
 
     # Show 1st and last point, help the user to follow better
     ax.scatter(0, 0, c='purple', edgecolors='red',
                s=60)                                      # beginning point (0,0)
     ax.scatter(rw.x_values[-1], rw.y_values[-1],          # Last point (since it start with 0, we do -1 to get the last number(example if it end at 5000, its actually 5001 so we do -1))
                c='orange', edgecolors='red', s=60)
+
+    ax.set_xlabel("Horizontal Walk (x axis)", fontsize=14)
+    ax.set_ylabel("Vertical Walk (y axis)", fontsize=14)
 
     # ax.get_xaxis().set_visible(False)                    (Remove X axis)
     # ax.get_yaxis().set_visible(False)                    (Remove Y axis)
