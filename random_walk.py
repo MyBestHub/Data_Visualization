cb8a61f368a1c3391da52db7482a1378be710125
Hussein Dabs <fifa_street_3000@hotmail.com>
2023-12-06 22:17:47 -0500

    NewX+Y(distance=2)

--- random_walk.py
+++ random_walk.py
@@ -13,11 +13,11 @@ class Random_walk():
         while len(self.x_values) < self.num_points:
 
             x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3])
+            x_distance = choice([0, 1, 2])
             x_step = x_direction * x_distance
 
             y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3])
+            y_distance = choice([0, 1, 2])
             y_step = y_direction * y_distance
 
             if x_step == 0 and y_step == 0:  # Skip the steps that go nowhere
